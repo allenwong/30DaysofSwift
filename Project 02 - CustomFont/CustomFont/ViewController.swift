d80b23462cf1881128556edf3789419e1abc11d7
Rishabh Tayal <rtayal11@gmail.com>
2016-08-03 11:36:29 -0500

    Project 2 swift 3 migration

--- Project 02 - CustomFont/CustomFont/ViewController.swift
+++ Project 02 - CustomFont/CustomFont/ViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
     var data = ["30 Days Swift", "这些字体特别适合打「奋斗」和「理想」", "谢谢「造字工房」，本案例不涉及商业使用", "使用到造字工房劲黑体，致黑体，童心体", "呵呵，再见🤗 See you next Project", "微博 @Allen朝辉"]
     
     var fontNames = ["MFTongXin_Noncommercial-Regular", "MFJinHei_Noncommercial-Regular", "MFZhiHei_Noncommercial-Regular"]
     
     var fontRowIndex = 0
- 
+    
     @IBOutlet weak var changeFontButton: UIButton!
     @IBOutlet weak var fontTableView: UITableView!
     @IBAction func changeFontDidTouch(sender: AnyObject) {
@@ -24,9 +24,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         fontTableView.reloadData()
         
     }
-  
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return UIStatusBarStyle.LightContent
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        get {
+            return UIStatusBarStyle.lightContent
+        }
     }
     
     override func viewDidLoad() {
@@ -35,8 +37,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         fontTableView.dataSource = self
         fontTableView.delegate = self
         
-        for family in UIFont.familyNames() {
-            for font in UIFont.fontNamesForFamilyName(family){
+        for family in UIFont.familyNames {
+            for font in UIFont.fontNames(forFamilyName: family){
                 print(font)
             }
         }
@@ -45,32 +47,32 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
     }
     
     
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    private func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-
-        let cell = fontTableView.dequeueReusableCellWithIdentifier("FontCell", forIndexPath: indexPath)
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = fontTableView.dequeueReusableCell(withIdentifier: "FontCell", for: indexPath)
+        
         let text = data[indexPath.row]
         
-            cell.textLabel?.text = text
-            cell.textLabel?.textColor = UIColor.whiteColor()
-            cell.textLabel?.font = UIFont(name:self.fontNames[fontRowIndex], size:16)
+        cell.textLabel?.text = text
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.font = UIFont(name:self.fontNames[fontRowIndex], size:16)
         
         return cell
-    
+        
     }
-
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
-
+    
 }
 
