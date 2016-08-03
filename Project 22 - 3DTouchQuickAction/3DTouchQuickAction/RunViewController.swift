acdc53276a80057f7da6647a061d256728d8cb67
Rishabh Tayal <rtayal11@gmail.com>
2016-08-03 12:08:41 -0500

    Project 22

--- Project 22 - 3DTouchQuickAction/3DTouchQuickAction/RunViewController.swift
+++ Project 22 - 3DTouchQuickAction/3DTouchQuickAction/RunViewController.swift
@@ -12,7 +12,7 @@ class RunViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        UIApplication.sharedApplication().statusBarHidden =  true
+        UIApplication.shared.isStatusBarHidden =  true
     }
 
     override func didReceiveMemoryWarning() {
