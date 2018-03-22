c60e1c037dc003bf4f5bb981ea711210206d640a
瞿炬星 <qujuxing@qujuxingdemacbook-pro.local>
2018-03-23 01:48:25 +0800

    修复了Project09 - Image Scroller 在Xcode 9下面无法实现缩放的错误。注意：1、Visual Effect View位置应在scrollView的上面，且opaque的属性应取消勾选，才能让scrollView做缩放imageView操作时，背景呈现模糊特效。2、Visual Effect View的bottom和top约束应设置为相对于superView的距离为0，而不是相对于safeArea。这样才能使模糊区域覆盖到系统的状态栏，不会出现白边。3.我参考了https://www.raywenderlich.com/159481/uiscrollview-tutorial-getting-started 的关于scrollView的scale写法，文章解释的很详细，我已翻译在viewController的注释中。

--- Project 09 -ImageScroller/ImageScrollerEffect/AppDelegate.swift
+++ Project 09 -ImageScroller/ImageScrollerEffect/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  ImageScrollerEffect
 //
-//  Created by Allen on 16/1/16.
-//  Copyright © 2016年 Allen. All rights reserved.
+//  Created by 瞿炬星 on 2018/3/21.
+//  Copyright © 2018年 瞿炬星. All rights reserved.
 //
 
 import UIKit
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
