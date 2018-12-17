3275058b37da02236dfa91cc058a9778ce0efb95
qianyb <qianyb@ctrip.com>
2018-12-17 21:43:23 +0800

    day2学习

--- Project 02 - CustomFont/CustomFont/ViewController.swift
+++ Project 02 - CustomFont/CustomFont/ViewController.swift
@@ -9,20 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    var data = ["30 Days Swift", "这些字体特别适合打「奋斗」和「理想」", "谢谢「造字工房」，本案例不涉及商业使用", "使用到造字工房劲黑体，致黑体，童心体", "呵呵，再见🤗 See you next Project", "微博 @Allen朝辉",
-                "测试测试测试测试测试测试",
-                "123",
-                "Alex",
-                "@@@@@@"]
     
-    var fontNames = ["MFTongXin_Noncommercial-Regular", "MFJinHei_Noncommercial-Regular", "MFZhiHei_Noncommercial-Regular", "Zapfino", "Gaspar Regular"]
+    static let identifier = "FontCell"
+    
+    var data = ["30 Days Swift", "这些字体特别适合打「奋斗」和「理想」",
+                "谢谢「造字工房」，本案例不涉及商业使用", "使用到造字工房劲黑体，致黑体，童心体",
+                "呵呵，再见🤗 See you next Project", "微博 @Allen朝辉",
+                "测试测试测试测试测试测试", "123", "Alex", "@@@@@@"]
+    
+    var fontNames = ["MFTongXin_Noncommercial-Regular",
+                     "MFJinHei_Noncommercial-Regular",
+                     "MFZhiHei_Noncommercial-Regular",
+                     "Zapfino",
+                     "Gaspar Regular"]
     
     var fontRowIndex = 0
  
-    @IBOutlet weak var changeFontButton: UIButton!
+    @IBOutlet weak var changeFontLabel: UILabel!
     @IBOutlet weak var fontTableView: UITableView!
-    @IBAction func changeFontDidTouch(_ sender: AnyObject) {
+    
+    func changeFontDidTouch(_ sender: AnyObject) {
         
         fontRowIndex = (fontRowIndex + 1) % 5
         print(fontNames[fontRowIndex])
@@ -36,38 +42,36 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 在storyboard中直接设置了
+        // fontTableView.dataSource = self
+        // fontTableView.delegate = self
         
-        fontTableView.dataSource = self
-        fontTableView.delegate = self
-        
-//        for family in UIFont.familyNames {
-//            for font in UIFont.fontNames(forFamilyName: family){
-//                print(font)
-//            }
-//        }
-        
-        changeFontButton.layer.cornerRadius = 50
-        
+        // 使用手势加Label替换button
+        changeFontLabel.layer.cornerRadius = 50
+        changeFontLabel.layer.masksToBounds = true
+        // 设置为true才能响应手势
+        changeFontLabel.isUserInteractionEnabled = true
+        let gesture = UITapGestureRecognizer(target: self,
+                                             action: #selector(changeFontDidTouch(_:)))
+        changeFontLabel.addGestureRecognizer(gesture)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 35
     }
     
-    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cell = fontTableView.dequeueReusableCell(withIdentifier: "FontCell", for: indexPath)
-    
+//        let cell = fontTableView.dequeueReusableCell(withIdentifier: ViewController.identifier)
+        let cell = fontTableView.dequeueReusableCell(withIdentifier: ViewController.identifier, for: indexPath)
         let text = data[indexPath.row]
         
         cell.textLabel?.text = text
         cell.textLabel?.textColor = UIColor.white
-        cell.textLabel?.font = UIFont(name:self.fontNames[fontRowIndex], size:16)
+        cell.textLabel?.font = UIFont(name: self.fontNames[fontRowIndex], size:16)
         
         return cell
     
