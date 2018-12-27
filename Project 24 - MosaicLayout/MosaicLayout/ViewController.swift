7593514c8eb56341d638b0829e54ac3067f619b6
qianyb <qianyb@ctrip.com>
2018-12-27 18:18:14 +0800

    第24天 - MosiacLayout，配合Pods的使用

--- Project 24 - MosaicLayout/MosaicLayout/ViewController.swift
+++ Project 24 - MosaicLayout/MosaicLayout/ViewController.swift
@@ -9,87 +9,72 @@
 import UIKit
 import FMMosaicLayout
 
-class ViewController: UICollectionViewController, FMMosaicLayoutDelegate {
+class ViewController: UICollectionViewController {
     
     var imageArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         imageArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"]
-        
-        let mosaicLayout : FMMosaicLayout = FMMosaicLayout()
+        let mosaicLayout = FMMosaicLayout()
         self.collectionView?.collectionViewLayout = mosaicLayout
-        
+        if #available(iOS 11.0, *) {
+            self.collectionView?.contentInsetAdjustmentBehavior = .never
+        }
     }
     
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return UIStatusBarStyle.LightContent
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    
-    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: FMMosaicLayout!, numberOfColumnsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 10
     }
     
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath)
-        
-        let randomBlue = CGFloat(drand48())
-        let randomRed = CGFloat(drand48())
-        let randomGreen = CGFloat(drand48())
-        
-        cell.backgroundColor = UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
         cell.alpha = 0
         
-        let imageView = cell.viewWithTag(2) as! UIImageView
+        let imageView = cell.contentView.viewWithTag(2) as! UIImageView
         imageView.image = UIImage(named: imageArray[indexPath.row])
         
-        
         let cellDelay = UInt64((arc4random() % 600 ) / 1000 )
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(cellDelay * NSEC_PER_SEC )), dispatch_get_main_queue(), ({ () -> Void in
-            
-            UIView.animateWithDuration(0.8, animations: ({
-                
+        let cellDelayTime = DispatchTime(uptimeNanoseconds: cellDelay * NSEC_PER_SEC)
+        DispatchQueue.main.asyncAfter(deadline: cellDelayTime) {
+            UIView.animate(withDuration: 0.8, animations: {
                 cell.alpha = 1.0
-                
-            }))
-            
-        }))
+            })
+        }
         
         return cell
-        
     }
 
-    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return imageArray.count
     }
+}
+
+extension ViewController : FMMosaicLayoutDelegate {
     
-    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: FMMosaicLayout!, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
-        
-        return UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
+    func collectionView(_ collectionView: UICollectionView!, layout collectionViewLayout: FMMosaicLayout!, numberOfColumnsInSection section: Int) -> Int {
+        return 3
     }
     
-    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: FMMosaicLayout!, interitemSpacingForSectionAtIndex section: Int) -> CGFloat {
+    func collectionView(_ collectionView: UICollectionView!, layout collectionViewLayout: FMMosaicLayout!, mosaicCellSizeForItemAt indexPath: IndexPath!) -> FMMosaicCellSize {
+        return indexPath.item % 7 == 0 ? .big : .small
+    }
+    
+    func collectionView(_ collectionView: UICollectionView!, layout collectionViewLayout: FMMosaicLayout!, interitemSpacingForSectionAt section: Int) -> CGFloat {
         return 1.0
     }
     
-    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: FMMosaicLayout!, mosaicCellSizeForItemAtIndexPath indexPath: NSIndexPath! ) -> FMMosaicCellSize {
-        return indexPath.item % 7 == 0 ? FMMosaicCellSize.Big : FMMosaicCellSize.Small
+    func collectionView(_ collectionView: UICollectionView!, layout collectionViewLayout: FMMosaicLayout!, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
     }
     
-    
-
-    
 }
-
